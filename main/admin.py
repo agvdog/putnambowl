15ff5b70a08849678ce3a2de0963baa956040a71
agvdog <76631362+agvdog@users.noreply.github.com>
2021-08-20 21:39:43 -0400

    Delete main directory

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-admin.site.register(models.Profile)
-admin.site.register(models.Gamemodel)
-admin.site.register(models.General)
-admin.site.register(models.History)
-
